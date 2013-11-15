892c258d8fc85acb8c560d7225d32c99001e2f06
Scott <scott.tolksdorf@gmail.com>
2013-11-15 17:30:29 -0500

    Updated xo-node, needs testing badly

--- xo-node.js
+++ xo-node.js
@@ -1,7 +1,23 @@
 
+var DEBUG = false;
+
 exports.endpoints = [];
 
-exports.api = function(endpoint, Model, middleware){
+//Removes the mongoose properties from the objects
+exports.clean = function(obj){
+	if(!obj) return obj;
+	if(typeof obj.length === 'number'){
+		return _.map(obj, function(obj){
+			return exports.clean(obj);
+		});
+	}
+	var result = obj.toObject();
+	delete result._id;
+	delete result.__v;
+	return result;
+};
+
+exports.api = function(endpoint, Model, middleware, handleError){
 	exports.endpoints.push(endpoint);
 	middleware = middleware || [];
 	var mw = {
@@ -18,68 +34,88 @@ exports.api = function(endpoint, Model, middleware){
 		mw.del  = middleware.del  || [];
 	}
 
-	//Removes the mongoose properties from the objects
-	var clean = function(obj){
-		if(typeof obj.length === 'number'){
-			return _.map(obj, function(obj){
-				return clean(obj);
-			});
-		}
-		var result = obj.toObject();
-		delete result._id;
-		delete result.__v;
-		return result;
+	handleError = handleError || function(err, req, res){
+		console.log('ERROR:', err);
+		res.send(500, err);
 	};
 
+	//XO Middleware
+	mw.findAll = function(req,res,next){
+		Model.find(function(err, models){
+			if(err) return handleError(err, req, res);
+			req.models = models;
+			return next();
+		});
+	};
+	mw.find = function(req,res,next){
+		Model.findById(req.params.id, function(err, obj){
+			if(err) return handleError(err, req, res);
+			req.model = obj;
+			return next();
+		});
+	};
+	mw.create = function(req,res,next){
+		req.model = new Model(req.body);
+		req.model.id = req.model._id;
+		return next();
+	};
+	mw.update = function(req,res,next){
+		Model.findById(req.params.id, function(err, obj){
+			if(!obj || err) return handleError(err, req, res);
+			req.model = _.extend(obj, req.body);
+			return next();
+		});
+	};
+
+
 	//Collection
-	app.get(endpoint, mw.get, function(req,res){
-		Model.find(function(err, objs){
-			console.log('getting all : ' + endpoint, clean(objs));
-			if(err) return res.send(500, err.message);
-			return res.send(clean(objs));
+	app.get(endpoint, mw.findAll, mw.get, function(req,res){
+		if(!req.models) return handleError('no collection', req, res);
+		return res.send(200, exports.clean(req.models));
+	});
+
+	app.delete(endpoint, mw.findAll, mw.del, function(req,res){
+		if(!req.models) return handleError('no collection', req, res);
+		Model.remove({}, function(err){
+			if(err) return handleError(err, req, res);
+			return res.send(200, []);
 		});
 	});
 
-	app.delete(endpoint, mw.del, function(req,res){
-		console.log('delete collection : '+ endpoint);
-		Model.remove({}, function(err){
-			if(err) return res.send(500, err.message);
+	app.put(endpoint, mw.findAll, mw.put, function(req,res){
+		if(!req.models) return handleError('no collection', req, res);
+
+		//TODO: Add mass collection update
+	});
+
+
+	//Model
+	app.get(endpoint + '/:id', mw.find, mw.get, function(req,res){
+		if(!req.model) return handleError('no model', req, res);
+		return res.send(200, exports.clean(req.model));
+	});
+
+	app.delete(endpoint + '/:id', mw.find, mw.del, function(req,res){
+		if(!req.model) return handleError('no model', req, res);
+		req.model.remove(function(err){
+			if(err) return handleError(err, req, res);
 			return res.send(200);
 		});
 	});
 
-	//Object
-	app.get(endpoint + '/:id', mw.get, function(req,res){
-		console.log('getting : '+ endpoint, req.params.id);
-		Model.findById(req.params.id, function(err, obj){
-			if(err) return res.send(500, err.message);
-			return res.send(clean(obj));
+	app.post(endpoint, mw.create, mw.post, function(req, res){
+		if(!req.model) return handleError('no model', req, res);
+		req.model.save(function(err, obj){
+			if(err) return handleError(err, req, res);
+			return res.send(200, exports.clean(obj));
 		});
 	});
 
-	app.post(endpoint, mw.post, function(req, res){
-		var obj = new Model(req.body);
-		if(!obj.id) obj.id = obj._id;
-		obj.save(function(err, obj){
-			if(err) return res.send(500, err.message);
-			console.log('creating : '+ endpoint, obj);
-			return res.send(clean(obj));
-		});
-	});
-
-	app.put(endpoint + '/:id', mw.put, function(req,res){
-		console.log('update : '+ endpoint, req.body);
-		Model.findByIdAndUpdate(req.params.id, req.body, function(err, obj){
-			if(err) return res.send(500, err.message);
-			return res.send(clean(obj));
-		});
-	});
-
-	app.delete(endpoint + '/:id', mw.del, function(req,res){
-		console.log('deleteing : '+ endpoint, req.params.id);
-		Model.findByIdAndRemove(req.params.id, function(err, obj){
-			if(err) return res.send(500, err.message);
-			return res.send(clean(obj));
+	app.put(endpoint + '/:id', mw.update, mw.put, function(req,res){
+		if(!req.model) return handleError('no model', req, res);
+		req.model.save(function(err, obj){
+			if(err) return handleError(err, req, res);
+			return res.send(200, exports.clean(obj));
 		});
 	});
 }
